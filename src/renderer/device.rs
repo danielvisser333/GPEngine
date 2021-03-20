c3fa6d5ee9e51d34d04b18618cad07fdb40ab065
Daniël Visser <danielvisser10@outlook.com>
2021-03-20 17:29:25 +0100

    Test2

--- src/renderer/device.rs
+++ src/renderer/device.rs
@@ -20,8 +20,11 @@ pub unsafe fn get_physical_device(instance : &Instance , surface_loader : &Surfa
     }
     return prefered_device.unwrap();
 }
-pub unsafe fn create_device(instance : &Instance , physical_device : &PhysicalDevice)->Device{
+pub unsafe fn create_device(instance : &Instance , physical_device : &PhysicalDevice , surface_loader : &Surface , surface : &SurfaceKHR)->Device{
     let extensions = [Swapchain::name().as_ptr()];
+    let graphics_queue_family = get_graphics_queue_family(instance, physical_device);
+    let presentation_queue_family = get_presentation_queue_family(instance, physical_device, surface_loader, surface, graphics_queue_family);
+    let 
     let features = PhysicalDeviceFeatures{
         ..Default::default()
     };
@@ -41,4 +44,28 @@ pub unsafe fn get_graphics_queue_family(instance : &Instance , physical_device :
         if queue_family.queue_flags.contains(QueueFlags::GRAPHICS){return i as u32}
     }
     panic!("No graphics operations supported on the GPU");
+}
+pub unsafe fn get_presentation_queue_family(instance : &Instance , physical_device : &PhysicalDevice , surface_loader : &Surface , surface : &SurfaceKHR , prefered_queue : u32)->u32{
+    let queue_families = instance.get_physical_device_queue_family_properties(*physical_device);
+    if surface_loader.get_physical_device_surface_support(*physical_device, prefered_queue, *surface).expect("Failed to check GPU surface support"){return prefered_queue}
+    for (i,_) in queue_families.iter().enumerate(){
+        if surface_loader.get_physical_device_surface_support(*physical_device, prefered_queue, *surface).expect("Failed to check GPU surface support"){return i as u32}
+    }
+    panic!("No presentation supported for GPU");
+}
+pub unsafe fn get_compute_queue_family(instance : &Instance , physical_device : &PhysicalDevice) -> u32{
+    let mut fallback_queue_family = None;
+    for (i,&queue_family) in instance.get_physical_device_queue_family_properties(*physical_device).iter().enumerate(){
+        if queue_family.queue_flags.contains(QueueFlags::COMPUTE) && !queue_family.queue_flags.contains(QueueFlags::GRAPHICS){return i as u32}
+        if fallback_queue_family.is_none() && queue_family.queue_flags.contains(QueueFlags::COMPUTE){fallback_queue_family = Some(i as u32)}
+    }
+    return fallback_queue_family.expect("No compute operations supported on the GPU");
+}
+pub unsafe fn get_transfer_queue_family(instance : &Instance , physical_device : &PhysicalDevice) -> u32{
+    let mut fallback_queue_family = None;
+    for (i,&queue_family) in instance.get_physical_device_queue_family_properties(*physical_device).iter().enumerate(){
+        if !queue_family.queue_flags.contains(QueueFlags::COMPUTE) && !queue_family.queue_flags.contains(QueueFlags::GRAPHICS) && queue_family.queue_flags.contains(QueueFlags::TRANSFER){return i as u32}
+        if fallback_queue_family.is_none() && queue_family.queue_flags.contains(QueueFlags::TRANSFER){fallback_queue_family = Some(i as u32)}
+    }
+    return fallback_queue_family.expect("No transfer operations supported on the GPU");
 }
